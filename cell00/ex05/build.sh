cda4ffc9c7e7de7bd2a0531757be768d2448e29b
huadchai <66051058@kmitl.ac.th>
2025-02-04 21:15:11 +0700

    Add files via upload

---
+++ cell00/ex05/build.sh
@@ -0,0 +1,8 @@
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    # วนลูปผ่านทุกอาร์กิวเมนต์และสร้างโฟลเดอร์ที่มี "ex" นำหน้า
+    for arg in "$@"; do
+        mkdir -p "ex$arg"
+    done
+fi
